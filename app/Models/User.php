6ffc8d3b8bbe9c7370a46b556772dc2eb14a0fdf
kimpatrickx <kimpatrickx@gmail.com>
2025-01-09 03:56:31 +0800

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'fname',
+        'lname',
+        'gender',
+        'birthdate',
+        'address',
+        'contactnum',
+        'email',
+        'role',
+        'imgf',
+        'imgb',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
